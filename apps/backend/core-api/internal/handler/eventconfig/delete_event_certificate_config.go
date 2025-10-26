ce5e1490cef5b97a5b86e736c5d825fcfa00c0d7
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2025-10-27 02:29:17 +0700

    fix: simplify error handling in DeleteEventCertificateConfig by returning the original error

--- apps/backend/core-api/internal/handler/eventconfig/delete_event_certificate_config.go
+++ apps/backend/core-api/internal/handler/eventconfig/delete_event_certificate_config.go
@@ -30,7 +30,7 @@ func (h *Handler) DeleteEventCertificateConfig(ctx *fiber.Ctx) error {
 
 	err = h.EventConfigUc.DeleteEventCertificateConfig(ctx.UserContext(), eventID)
 	if err != nil {
-		return customerror.Parse(&customerror.ErrInternalServer, err)
+		return err
 	}
 
 	return ctx.Status(http.StatusOK).JSON(map[string]string{"message": "Event certificate config deleted successfully"})
