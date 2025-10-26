95f7d5391f4bf6c33bb900086fb83d945106131d
DrowningToast <supratouchgussii@gmail.com>
2025-10-27 00:43:12 +0700

    feat: base layout

--- apps/web/src/components/layouts/navigations/PublicNavbar.tsx
+++ apps/web/src/components/layouts/navigations/PublicNavbar.tsx
@@ -1,31 +1,25 @@
 import { cn } from '@/lib/utils';
-import { ThemisWhite } from '@/components/icons';
+import { ThemisBlack, ThemisWhite } from '@/components/icons';
 
 interface PublicNavbarProps {
-    variant?: "primary" | "dark" | "light";
     className?: string;
+    variant?: "light" | "dark";
 }
 
-export function PublicNavbar({ variant = "primary", className }: PublicNavbarProps) {
-
-    // Define navbar styles based on variant
-    const variantStyles = {
-        primary: 'bg-[#eb5331]',
-        'dark': 'bg-secondary-foreground',
-        'light': 'bg-foreground',
-    };
-
+export function PublicNavbar({ className, variant = "light" }: PublicNavbarProps) {
     return (
         <nav className={cn(
-            'fixed top-0 left-0 right-0 z-50 transition-all',
-            variantStyles[variant],
+            'fixed top-0 left-0 right-0 z-50 transition-all bg-foreground-alt',
+            cn({
+                'bg-white/10': variant === 'light',
+            }),
             className
         )}>
             <div className="flex items-center justify-center px-4 py-1.5 md:px-8 md:py-4 max-w-[1512px] mx-auto">
                 {/* Logo */}
                 <a href="/" className="flex items-center transition-transform hover:scale-105">
                     <div className='size-9 md:size-12 grid place-items-center relative'>
-                        <ThemisWhite className='w-full h-full' />
+                        {variant === "light" ? <ThemisWhite className='w-full h-full' /> : <ThemisBlack className='w-full h-full' />}
                     </div>
                 </a>
             </div>
