92800cf0c12ee4ac1cc4aa1fb9eb38396ede03cd
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2025-11-09 08:10:34 +0700

    wip

--- apps/backend/core-api/internal/datagateway/event/event_issuer.go
+++ apps/backend/core-api/internal/datagateway/event/event_issuer.go
@@ -14,4 +14,5 @@ type EventIssuerDataGateway interface {
 	UpdateEventIssuer(ctx context.Context, params generated.UpdateEventIssuerParams) (*generated.EventIssuer, error)
 	DeleteEventIssuer(ctx context.Context, eventID uuid.UUID) error
 	GetEventIssuerByEventIDAndIssuerCredentialID(ctx context.Context, eventID uuid.UUID, issuerCredentialID uuid.UUID) (generated.EventIssuer, error)
+	UpdateEventIssuerSigningStatus(ctx context.Context, eventID uuid.UUID, issuerCredentialID uuid.UUID, isSigned int32) error
 }
