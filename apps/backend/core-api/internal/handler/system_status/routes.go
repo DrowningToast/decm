7913f36bfb6780adadbc726d35b6fa1ff86dddde
Supratouch Suwatno <supratouchgussii@gmail.com>
2025-12-18 23:24:29 +0700

    feat: system status handler and usecase

---
+++ apps/backend/core-api/internal/handler/system_status/routes.go
@@ -0,0 +1,18 @@
+package system_status
+
+import (
+	"apps/backend/common/log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h *Handler) Mount(r fiber.Router) {
+	logger := log.LoadLogger()
+	defer logger.Info("Mounted system status routes")
+
+	systemStatusGroup := r.Group("/system-status")
+
+	systemStatusGroup.Get("/latest", h.GetLatestSchedules)
+	systemStatusGroup.Get("/period", h.GetSchedulesBetween)
+	systemStatusGroup.Get("/closest-incoming", h.GetClosestIncomingSchedule)
+}
